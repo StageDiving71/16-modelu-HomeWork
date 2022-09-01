ad62bad87d5a1d70127749988088984003c1f458
StageDiving71 <vova.byrlinov@gmail.com>
2022-09-01 12:14:13 +0300

    add some comment

---
+++ Git homework16/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Git homework16
+//
+//  Created by Владимир Бурлинов on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // asdasd
+        //asdasd
+    }
+
+
+}
+
