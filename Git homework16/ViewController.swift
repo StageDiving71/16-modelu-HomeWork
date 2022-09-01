d7918e53720994c608a8219548597b2eb29beee8
StageDiving71 <vova.byrlinov@gmail.com>
2022-09-01 12:15:45 +0300

    commit2

--- Git homework16/ViewController.swift
+++ Git homework16/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         // asdasd
         //asdasd
+        
+        //commit2: seqweqwe
     }
 
 
